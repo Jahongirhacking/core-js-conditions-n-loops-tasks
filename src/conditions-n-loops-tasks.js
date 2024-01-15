17000cd263c55f5aece7c55e166a19f3e5354fb7
Jahongirhacking <Jahongirhacking@gmail.com>
2024-01-15 15:37:54 +0500

    feat: add third task - loops

---
+++ src/conditions-n-loops-tasks.js
@@ -0,0 +1,566 @@
+/**
+ * Determines whether a given number is positive. Zero is considered positive.
+ * This function does not use Number or Math class methods.
+ *
+ * @param {number} number - The number to check.
+ * @return {boolean} True if the number is positive or zero, false otherwise.
+ *
+ * @example:
+ *  10 => true
+ *  0  => true
+ *  -5 => false
+ */
+function isPositive(number) {
+  return number >= 0;
+}
+
+/**
+ * Returns the maximum of three numbers without using Array and Math classes methods.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @param {number} c - The third number.
+ * @return {number} The maximum of the three numbers.
+ *
+ * @example:
+ *  1, 2, 3       => 3
+ *  -5, 0, 5      => 5
+ *  -0.1, 0, 0.2  => 0.2
+ */
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) return a;
+  if (b > a && b > c) return b;
+  return c;
+}
+
+/**
+ * Checks if a queen can capture a king in the next move on an 8x8 chessboard.
+ * See more details at https://en.wikipedia.org/wiki/Queen_(chess)
+ *
+ * @typedef {{
+ *  x: number,
+ *  y: number
+ * }} Position
+ * @param {Object} queen - The position of the queen.
+ * @param {Object} king - The position of the king.
+ * @return {boolean} True if the queen can capture the king, false otherwise.
+ *
+ * @example
+ * {x: 1, y: 1}, {x: 5, y: 5} => true
+ * {x: 2, y: 1}, {x: 2, y: 8} => true
+ * {x: 1, y: 1}, {x: 2, y: 8} => false
+ * {x: 1, y: 1}, {x: 2, y: 8} => false
+ */
+function canQueenCaptureKing(queen, king) {
+  return (
+    queen.x - king.x === 0 ||
+    queen.y - king.y === 0 ||
+    Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)
+  );
+}
+
+/**
+ * Determines whether a triangle is isosceles based on its side lengths.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {number} a - The length of the first side.
+ * @param {number} b - The length of the second side.
+ * @param {number} c - The length of the third side.
+ * @return {boolean} True if the triangle is isosceles, false otherwise.
+ *
+ * @example:
+ *  1, 2, 3   => false
+ *  3, 1, 2   => false
+ *  2, 3, 2   => true
+ *  3, 2, 2   => true
+ *  2, 2, 3   => true
+ *  2, 2, 5   => false
+ *  3, 0, 3   => false
+ */
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || Math.abs(a - b) >= c) return false;
+  return a === b || a === c || b === c;
+}
+
+/**
+ * Converts a number to Roman numerals. The number will be between 1 and 39.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {number} num - The number to convert.
+ * @return {string} The Roman numeral representation of the number.
+ *
+ * @example:
+ *  1   => I
+ *  2   => II
+ *  5   => V
+ *  10  => X
+ *  26  => XXVI
+ */
+function convertToRomanNumerals(num) {
+  switch (num) {
+    case 1:
+      return 'I';
+    case 2:
+      return 'II';
+    case 3:
+      return 'III';
+    case 4:
+      return 'IV';
+    case 5:
+      return 'V';
+    case 6:
+      return 'VI';
+    case 7:
+      return 'VII';
+    case 8:
+      return 'VIII';
+    case 9:
+      return 'IX';
+    case 10:
+      return 'X';
+    case 11:
+      return 'XI';
+    case 12:
+      return 'XII';
+    case 13:
+      return 'XIII';
+    case 14:
+      return 'XIV';
+    case 15:
+      return 'XV';
+    case 16:
+      return 'XVI';
+    case 17:
+      return 'XVII';
+    case 18:
+      return 'XVIII';
+    case 19:
+      return 'XIX';
+    case 20:
+      return 'XX';
+    case 21:
+      return 'XXI';
+    case 22:
+      return 'XXII';
+    case 23:
+      return 'XXIII';
+    case 24:
+      return 'XXIV';
+    case 25:
+      return 'XXV';
+    case 26:
+      return 'XXVI';
+    case 27:
+      return 'XXVII';
+    case 28:
+      return 'XXVIII';
+    case 29:
+      return 'XXIX';
+    case 30:
+      return 'XXX';
+    case 31:
+      return 'XXXI';
+    case 32:
+      return 'XXXII';
+    case 33:
+      return 'XXXIII';
+    case 34:
+      return 'XXXIV';
+    case 35:
+      return 'XXXV';
+    case 36:
+      return 'XXXVI';
+    case 37:
+      return 'XXXVII';
+    case 38:
+      return 'XXXVIII';
+    case 39:
+      return 'XXXIX';
+    default:
+      return 'Number out of range (1-39)';
+  }
+}
+
+/**
+ * Converts a number to a string, replacing digits with words.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {string} numberStr - The number as a string.
+ * @return {string} The number with digits replaced by words.
+ *
+ * @example:
+ *  '1'       => 'one'
+ *  '10'      => 'one zero'
+ *  '-10'     => 'minus one zero'
+ *  '10.5'    => 'one zero point five'
+ *  '10,5'    => 'one zero point five'
+ *  '1950.2'  => 'one nine five zero point two'
+ */
+function convertNumberToString(numberStr) {
+  let res = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const num = numberStr[i];
+    if (i !== 0) res += ' ';
+    switch (num) {
+      case '0':
+        res += 'zero';
+        break;
+      case '1':
+        res += 'one';
+        break;
+      case '2':
+        res += 'two';
+        break;
+      case '3':
+        res += 'three';
+        break;
+      case '4':
+        res += 'four';
+        break;
+      case '5':
+        res += 'five';
+        break;
+      case '6':
+        res += 'six';
+        break;
+      case '7':
+        res += 'seven';
+        break;
+      case '8':
+        res += 'eight';
+        break;
+      case '9':
+        res += 'nine';
+        break;
+      case '.':
+      case ',':
+        res += 'point';
+        break;
+      case '-':
+        res += 'minus';
+        break;
+      default:
+        break;
+    }
+  }
+
+  return res;
+}
+
+/**
+ * Determines whether a string is a palindrome.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {string} str - The string to check.
+ * @return {boolean} True if the string is a palindrome, false otherwise.
+ *
+ * @example:
+ *  'abcba'     => true
+ *  '0123210'   => true
+ *  'qweqwe'    => false
+ */
+function isPalindrome(str) {
+  for (let i = 0; i < Math.floor(str.length / 2); i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) return false;
+  }
+  return true;
+}
+
+/**
+ * Finds the first occurrence of a letter in a string.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {string} str - The string to search.
+ * @param {string} letter - The letter to find.
+ * @return {number} The index of the first occurrence of the letter, or -1 if not found.
+ *
+ * @example:
+ *  'qwerty', 'q'     => 0
+ *  'qwerty', 'е'     => 2
+ *  'qwerty', 'Q'     => -1
+ *  'qwerty', 'p'     => -1
+ */
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
+}
+
+/**
+ * Checks if a number contains a specific digit.
+ * In this task, the use of methods of the String and Array classes is not allowed.
+ *
+ * @param {number} num - The number to check.
+ * @param {number} digit - The digit to search for.
+ * @return {boolean} True if the number contains the digit, false otherwise.
+ *
+ * @example:
+ *  123450, 5   => true
+ *  123450, 1   => true
+ *  123450, 0   => true
+ *  12345, 0    => false
+ *  12345, 6    => false
+ */
+function isContainNumber(num, digit) {
+  for (let n = num; n !== 0; n = Math.floor(n / 10)) {
+    if (n % 10 === digit) return true;
+  }
+  return false;
+}
+
+/**
+ * Finds the index of an element in an array where the sum of elements to the left equals the sum of elements to the right.
+ * If such an index does not return -1.
+ * In this task, the use of methods of the Array and String classes is not allowed.
+ *
+ * @param {number[]} arr - The array to check.
+ * @return {number} The index of the balance point, or -1 if none exists.
+ *
+ * @example:
+ *  [1, 2, 5, 3, 0] => 2    => 1 + 2 === 3 + 0 then balance element is 5 and its index = 2
+ *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
+ *  [1, 2, 3, 4, 5] => -1   => no balance element
+ */
+function getBalanceIndex(arr) {
+  let sum = 0;
+  let prefixSum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
+  for (let i = 0; i < arr.length; i += 1) {
+    if (prefixSum === sum - arr[i] - prefixSum) {
+      return i;
+    }
+    prefixSum += arr[i];
+  }
+  return -1;
+}
+
+/**
+ * Generates a spiral matrix of a given size, filled with numbers in ascending order starting from one.
+ * The direction of filling with numbers is clockwise.
+ * Usage of String and Array classes methods is not allowed in this task.
+ *
+ * @param {number} size - The size of the matrix.
+ * @return {number[][]} The spiral matrix.
+ *
+ * @example:
+ *        [
+ *          [1, 2, 3],
+ *  3  =>   [8, 9, 4],
+ *          [7, 6, 5]
+ *        ]
+ *        [
+ *          [1,  2,  3,  4],
+ *  4  =>   [12, 13, 14, 5],
+ *          [11, 16, 15, 6],
+ *          [10, 9,  8,  7]
+ *        ]
+ */
+function getSpiralMatrix(size) {
+  const move = { y: 0, x: 1 };
+  const location = { y: 0, x: 0 };
+  const arr = new Array(size);
+  for (let i = 0; i < size; i += 1) {
+    arr[i] = new Array(size);
+  }
+  let flag = true;
+  for (let i = 0; i < size ** 2; i += 1) {
+    arr[location.y][location.x] = i + 1;
+    if (
+      location.y + move.y >= size ||
+      location.y + move.y < 0 ||
+      location.x + move.x >= size ||
+      location.x + move.x < 0 ||
+      arr[location.y + move.y][location.x + move.x]
+    ) {
+      if (flag) {
+        move.y = move.x;
+        move.x = 0;
+      } else {
+        move.x = -move.y;
+        move.y = 0;
+      }
+      flag = !flag;
+    }
+    location.x += move.x;
+    location.y += move.y;
+  }
+  return arr;
+}
+
+/**
+ * Rotates a matrix by 90 degrees clockwise in place.
+ * Take into account that the matrix size can be very large. Consider how you can optimize your solution.
+ * Usage of String and Array class methods is not allowed in this task.
+ *
+ * @param {number[][]} matrix - The matrix to rotate.
+ * @return {number[][]} The rotated matrix.
+ *
+ * @example:
+ *  [                 [
+ *    [1, 2, 3],        [7, 4, 1],
+ *    [4, 5, 6],  =>    [8, 5, 2],
+ *    [7, 8, 9]         [9, 6, 3]
+ *  ]                 ]
+ */
+function rotateMatrix(matrix) {
+  const fakeClone = matrix;
+  let arr = [];
+  for (let j = 0; j < matrix.length; j += 1) {
+    let temp = [];
+    for (let i = matrix.length - 1; i >= 0; i -= 1) {
+      temp = [...temp, matrix[i][j]];
+    }
+    arr = [...arr, [...temp]];
+  }
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr[0].length; j += 1) {
+      fakeClone[i][j] = arr[i][j];
+    }
+  }
+  return fakeClone;
+}
+
+/**
+ * Sorts an array of numbers in ascending order in place.
+ * Employ any sorting algorithm of your choice.
+ * Take into account that the array can be very large. Consider how you can optimize your solution.
+ * In this task, the use of methods of the Array and String classes is not allowed.
+ *
+ * @param {number[]} arr - The array to sort.
+ * @return {number[]} The sorted array.
+ *
+ * @example:
+ *  [2, 9, 5]       => [2, 5, 9]
+ *  [2, 9, 5, 9]    => [2, 5, 9, 9]
+ *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
+ */
+function sortByAsc(arr) {
+  const clone = arr;
+  const map = new Map();
+  for (let i = 0; i < clone.length; i += 1) {
+    const n = arr[i];
+    if (map.has(n)) map.set(n, map.get(n) + 1);
+    else map.set(n, 1);
+  }
+  let cnt = 0;
+  for (let i = -100; i <= 100; i += 1) {
+    if (map.has(i)) {
+      for (let j = 0; j < map.get(i); j += 1) {
+        clone[cnt] = i;
+        cnt += 1;
+      }
+    }
+  }
+  return clone;
+}
+
+/**
+ * Shuffles characters in a string so that the characters with an odd index are moved to the end of the string at each iteration.
+ * Take into account that the string can be very long and the number of iterations is large. Consider how you can optimize your solution.
+ * Usage of Array class methods is not allowed in this task.
+ *
+ * @param {string} str - The string to shuffle.
+ * @param {number} iterations - The number of iterations to perform the shuffle.
+ * @return {string} The shuffled string.
+ *
+ * @example:
+ *  '012345', 1 => '024135', '043215', '031425', '012345'
+ *  'qwerty', 1 => 'qetwry'
+ *  '012345', 2 => '024135' => '043215'
+ *  'qwerty', 2 => 'qetwry' => 'qtrewy'
+ *  '012345', 3 => '024135' => '043215' => '031425'
+ *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
+ */
+function shuffleChar(str, iterations) {
+  let counter = iterations;
+  let temp = str;
+  const etalon = str;
+  let flagInputStringEquality = false;
+
+  for (let i = 0; i < counter; i += 1) {
+    for (let j = 1; j < temp.length / 2 + 1; j += 1) {
+      const char = temp[j];
+      temp = temp.substring(0, j) + temp.substring(j + 1);
+      temp += char;
+    }
+
+    if (temp === etalon && !flagInputStringEquality) {
+      flagInputStringEquality = true;
+      const randomFactor = Math.floor(counter / (i + 1));
+      counter -= randomFactor * (i + 1);
+      i = -1;
+    }
+  }
+
+  return temp;
+}
+
+/**
+ * Returns the nearest largest integer consisting of the digits of the given positive integer.
+ * If there is no such number, it returns the original number.
+ * Usage of String class methods is not allowed in this task.
+ *
+ * @example:
+ * 12345    => 12354
+ * 123450   => 123504
+ * 12344    => 12434
+ * 123440   => 124034
+ * 1203450  => 1203504
+ * 90822    => 92028
+ * 321321   => 322113
+ *
+ * @param {number} number The source number
+ * @returns {number} The nearest larger number, or original number if none exists.
+ */
+function getNearestBigger(number) {
+  const digits = [];
+  let temp = number;
+  while (temp > 0) {
+    digits.unshift(temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+
+  let i = digits.length - 2;
+  while (i >= 0 && digits[i] >= digits[i + 1]) {
+    i -= 1;
+  }
+
+  if (i >= 0) {
+    let j = digits.length - 1;
+    while (digits[j] <= digits[i]) {
+      j -= 1;
+    }
+    [digits[i], digits[j]] = [digits[j], digits[i]];
+
+    const rightPart = digits.splice(i + 1);
+    digits.push(...rightPart.sort((a, b) => a - b));
+  }
+
+  let result = 0;
+  for (let k = 0; k < digits.length; k += 1) {
+    result = result * 10 + digits[k];
+  }
+
+  return result === number ? number : result;
+}
+
+module.exports = {
+  isPositive,
+  getMaxNumber,
+  canQueenCaptureKing,
+  isIsoscelesTriangle,
+  convertToRomanNumerals,
+  convertNumberToString,
+  isPalindrome,
+  getIndexOf,
+  isContainNumber,
+  getBalanceIndex,
+  getSpiralMatrix,
+  rotateMatrix,
+  sortByAsc,
+  shuffleChar,
+  getNearestBigger,
+};
